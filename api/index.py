458b1394ac9280b23674af9efdfdd814821877fc
bolojutsu <200mshinobi@gmail.com>
2026-01-10 15:39:53 -0500

    changes to deploy on vercel

--- api/index.py
+++ api/index.py
@@ -158,7 +158,3 @@ def get_history(coin_id):
         return jsonify(data.get("prices", []))
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
